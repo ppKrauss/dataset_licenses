66d5f60c56afa45da0ec5280ca328cdf5ac55a83
ppkrauss <ppkrauss@gmail.com>
2015-09-21 21:36:54 +0000

    adding preparation algotithm and a first CSV

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # dataset_licenses
 proposal for CSV of licenses, see https://github.com/datasets/registry/issues/118
+
+The [licenses.csv](./data/licenses.csv) have all licenses, [datapackage.json](./datapackage.json) its metadata.
+
+## Preparation
+
+1. With `php csv-conv.php > temp.csv` generates a fresh linceses.csv from each json at https://github.com/okfn/licenses/tree/master/licenses 
+
+2. open it in [the collaborative worksheet](https://docs.google.com/spreadsheets/d/17RwlPayXj2IBIBszp4wKMdK7OwwPqX125WmF3XFzM0A/edit?usp=sharing), where the column "family" is in use and a field url_original was added.
+
+3. save as CSV here.
+
+NOTE: to check and compare news on JSON or two CSVs, there are an adaptation of csv-conv.php to do this tasks.  
+
---
+++ csv-conv.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Convert JSON-OKFN-licenses into CSV. See https://github.com/okfn/licenses
+ * @version 2015-09-21
+ * @usage php csv-conv.php > test.csv
+ */
+
+///CONFIGS:
+	$dir= '../licenses/licenses'; // obtained from clone https://github.com/okfn/licenses
+    $fileOut = 'php://stdout';
+    $attlist = 'id_label,id_version,id,family,canonic,status,is_generic,domain_content,domain_data,domain_software,od_conformance,osd_conformance,maintainer,title,url';
+    $check_mode = 0;  // 1 to check header composition
+
+/// BEGIN:
+$fp = fopen($fileOut,'a');
+$attlist = explode(',',$attlist);
+$n=0;
+$checKeys = [];
+fputcsv($fp,$attlist); // print CSV line
+foreach(scandir($dir) as $file) if (preg_match('/^(.+)\.json$/',$file,$m)) {
+    $metadata = json_decode( file_get_contents("$dir/$file"), TRUE ); // get a JSON file
+    $filtered = [];
+    foreach($attlist as $name) // standard sequence
+        $filtered[] = isset($metadata[$name])? $metadata[$name]: '';
+    $label = strtolower(str_replace('.json','',$file)); // lc_label
+    $filtered[0] = ($label=='cc0' || $label=='w3c')? $label: preg_replace('/\d\-|\-\d|[\d\.]+/','',$label);
+    if (preg_match('/[\d\.]+/',$label,$m))
+        $filtered[1] = "$m[0] "; // workaround to preserve decimal
+    $key = $filtered[0] .'-'. trim($filtered[1]);
+    if ($check_mode) {
+        $aux = array_keys($metadata);
+        sort($aux);
+        $aux = join(",",$aux);
+        if (!isset($checKeys[$aux])) $checKeys[$aux] = 1;
+    } else
+        fputcsv($fp, $filtered);  // prints CSV line 
+    $n++;
+}
+if ($check_mode) foreach(array_keys($checKeys) as $k)
+    print "\n--- $k";
+?>
